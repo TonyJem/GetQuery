18980aad1644e76235a2632d91679dc2116e0edf
TonyJem <a.jemanov@gmail.com>
2021-04-28 22:27:03 +0300

    Show respondData in TextView and Show activity indicator while loading

--- GetQueryApp/GetQueryApp/ViewController.swift
+++ GetQueryApp/GetQueryApp/ViewController.swift
@@ -3,10 +3,24 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet private weak var postTextView: UITextView!
     @IBOutlet private weak var postIDTextField: UITextField!
+    @IBOutlet private weak var indicatorView: UIView!
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+    
+    private var respondedData = "" {
+        didSet {
+            postTextView.text = respondedData
+        }
+    }
+    
+    private var session: URLSession {
+        let configuration = URLSessionConfiguration.default
+        return URLSession(configuration: configuration)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        indicatorView.isHidden = true
         postTextView.text = ""
         postIDTextField.delegate = self
     }
@@ -18,6 +32,7 @@ class ViewController: UIViewController {
             postID.remove(at: postID.startIndex)
             postIDTextField.text = postID
         }
+        turnActivityIndicatorON()
         getPosts(with: postID)
     }
 }
@@ -27,6 +42,7 @@ extension ViewController: UITextFieldDelegate {
         guard CharacterSet(charactersIn: "0123456789").isSuperset(of: CharacterSet(charactersIn: string)) else {
             return false
         }
+        respondedData = ""
         let currentText = textField.text ?? ""
         
         guard let stringRange = Range(range, in: currentText) else { return false }
@@ -39,19 +55,35 @@ private extension ViewController {
     func getPosts(with ID: String) {
         let url = URL(string: "https://jsonplaceholder.typicode.com/comments?postId=\(ID)")
         
-        guard let requestUrl = url else { return }
+        guard let requestUrl = url else {
+            self.turnActivityIndicatorOFF()
+            return }
         var request = URLRequest(url: requestUrl)
         request.httpMethod = "GET"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            guard let data = data,
-                  let dataString = String(data: data, encoding: .utf8),
-                  (response as? HTTPURLResponse)?.statusCode == 200,
-                  error == nil else { return }
-//            self.postTextView.text = dataString
-            print("🟢 \(dataString)")
+            DispatchQueue.main.async {
+                guard let data = data,
+                      let dataString = String(data: data, encoding: .utf8),
+                      (response as? HTTPURLResponse)?.statusCode == 200,
+                      error == nil else {
+                    self.turnActivityIndicatorOFF()
+                    return }
+                self.turnActivityIndicatorOFF()
+                self.respondedData = dataString
+            }
         }
         task.resume()
+    }
+    
+    func turnActivityIndicatorON() {
+        indicatorView.isHidden = false
+        activityIndicator.startAnimating()
+    }
+    
+    func turnActivityIndicatorOFF() {
+        indicatorView.isHidden = true
+        activityIndicator.stopAnimating()
     }
 }
